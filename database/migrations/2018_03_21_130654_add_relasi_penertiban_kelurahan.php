2c7ada929210ced69ccffc74d5d24e9a4a4155c8
royirwan <roy.irwan@gmail.com>
2018-03-21 14:27:35 +0700

    Cuman nambah comment doang, test pindah ke bitbucket  Changes to be committed: 	modified:   database/migrations/2018_03_21_130654_add_relasi_penertiban_kelurahan.php

--- database/migrations/2018_03_21_130654_add_relasi_penertiban_kelurahan.php
+++ database/migrations/2018_03_21_130654_add_relasi_penertiban_kelurahan.php
@@ -14,6 +14,7 @@ class AddRelasiPenertibanKelurahan extends Migration
     public function up()
     {
         Schema::table('penertiban', function (Blueprint $table) {
+            //nambah relasi ke table kelurahan
             $table->foreign('kelurahan_id')
                 ->references('id')
                 ->on('kelurahan');
